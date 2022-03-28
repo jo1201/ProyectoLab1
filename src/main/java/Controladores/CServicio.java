4579ce212dfff70b7af70065cb76585beae8a0df
fernando <fernando19.asj@gmail.com>
2022-03-27 21:11:32 -0500

    Se crearon Mensajes, se creo la vista de servicio, se validaron algunas acciones

---
+++ src/main/java/Controladores/CServicio.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controladores;
+
+import Generales.Mensajes;
+import Modelos.MServicio;
+import Vistas.VServicio;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+/**
+ *
+ * @author Khaal
+ */
+public class CServicio implements ActionListener {
+    
+    private VServicio vserv;
+    private Mensajes msj = new Mensajes();
+    private CMenu cmenu;
+    private MServicio mserv;
+    public CServicio(CMenu cmenu){
+        vserv = new VServicio(cmenu.getVMenu(),false,null)
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
--- src/main/java/Modelos/MServicio.java
+++ src/main/java/Modelos/MServicio.java
@@ -9,5 +9,46 @@ package Modelos;
  * @author Khaal
  */
 public class MServicio {
+    private String codigo,nombre,tipo;
+    private float costo;
+    
+    public MServicio(){}
+
+    public float getCosto() {
+        return costo;
+    }
+
+    public void setCosto(float costo) {
+        this.costo = costo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+ 
+    
+    
+    
     
 }
